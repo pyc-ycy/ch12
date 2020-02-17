4fe8fbc3d0fd0c27c597a317c2b86a6a5b509de7
ycy-ycy <2923616405@qq.com>
2020-02-17 11:35:53 +0800

    Initial commit

---
+++ src/main/java/com/pyc/ch12/CloudNativeAppApplication.java
@@ -0,0 +1,13 @@
+package com.pyc.ch12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pyc/ch12/CloudNativeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyc.ch12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
